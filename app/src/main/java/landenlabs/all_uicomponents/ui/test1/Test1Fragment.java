67b6e256a56a5d12bea763f55fca96726e3b05d3
Dennis Lang <landenlabs2@gmail.com>
2025-06-03 15:38:43 -0400

    first post

---
+++ app/src/main/java/landenlabs/all_uicomponents/ui/test1/Test1Fragment.java
@@ -0,0 +1,37 @@
+package landenlabs.all_uicomponents.ui.test1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import landenlabs.all_uicomponents.databinding.FragmentTest1Binding;
+
+public class Test1Fragment extends Fragment {
+
+    private FragmentTest1Binding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        landenlabs.all_uicomponents.ui.test1.Test1ViewModel test1ViewModel =
+                new ViewModelProvider(this).get(landenlabs.all_uicomponents.ui.test1.Test1ViewModel.class);
+
+        binding = FragmentTest1Binding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.test1Title;
+        test1ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
